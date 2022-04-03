9c97c10d201ff1a10169fc9126813ad9e9d6632b
Astitva Gupta <gupta.astitva@gmail.com>
2022-04-03 16:12:40 +0530

    Updated the UI of the details page

---
+++ frontend/src/components/Tabs/Versions.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Versions extends Component {
+  render() {
+    return (
+      <div> Versions Component </div>
+    );
+  }
+}
+
+export default Versions;
