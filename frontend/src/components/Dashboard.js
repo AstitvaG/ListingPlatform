755b471f30ee8430a1814eed6f0f0cbaafc148ed
baroood <22sidj@gmail.com>
2022-04-30 19:45:21 +0530

    fixed dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -8,54 +8,50 @@ export default class ProfilePage extends Component {
     super(props);
 
     this.state = {
-      datasetUploads: [
+      datasets:
+      {
+        "1":
         {
           id: "1",
           name: "DATASET1",
           description:
             'Alternate bind version (for css-modules If you are using css-modules, or a similar approach to abstract class "names" and the real className values that are actually output to the DOM, you may want to use the bind variant',
         },
+        "2":
         {
           id: "2",
           name: "DATASET2",
           description:
             'Alternate bind version (for css-modules If you are using css-modules, or a similar approach to abstract class "names" and the real className values that are actually output to the DOM, you may want to use the bind variant',
         },
-      ],
-      datasetSubscriptions: [
-        {
-          id: "1",
-          name: "DATASET1",
-          description:
-            'Alternate bind version (for css-modules If you are using css-modules, or a similar approach to abstract class "names" and the real className values that are actually output to the DOM, you may want to use the bind variant',
-        },
-        {
-          id: "2",
-          name: "DATASET2",
-          description:
-            'Alternate bind version (for css-modules If you are using css-modules, or a similar approach to abstract class "names" and the real className values that are actually output to the DOM, you may want to use the bind variant',
-        },
+        "3":
         {
           id: "3",
           name: "DATASET3",
           description:
             'Alternate bind version (for css-modules If you are using css-modules, or a similar approach to abstract class "names" and the real className values that are actually output to the DOM, you may want to use the bind variant',
         },
+      },
+      uploads: [
+      "1","2",
       ],
       subscriptions: {
         
         "1":
         {
+          id: "1",
           startDate: "2-10-22",
           endDate: "10-10-22",
         },
         "2":
         {
+          id: "2",
           startDate: "2-10-22",
           endDate: "20-10-22",
         },
         "3":
         {
+          id: "3",
           startDate: "2-10-22",
           endDate: "30-10-22",
         },
@@ -71,7 +67,7 @@ export default class ProfilePage extends Component {
           <div className="d-flex justify-content-around h3 row">
           My Uploads
           </div>
-            {this.state.datasetUploads.map((datasetId, i) => {
+            {this.state.uploads.map((datasetId, i) => {
               return (
                 <div style={{ padding: "1rem" }}>
                   <div
@@ -89,11 +85,11 @@ export default class ProfilePage extends Component {
                     >
                       <div className="d-flex row">
                         <h5 style={{ flex: 1, padding: "0.5rem" }}>
-                          {datasetId.name}
+                          {this.state.datasets[datasetId].name}
                         </h5>
                         <div className="d-flex flex-row-reverse">
                           <Link
-                            to={`/details/${datasetId.name}`}
+                            to={`/details/${this.state.datasets[datasetId].name}`}
                             className="btn btn-secondary"
                             style={{
                               borderRadius: "20px",
@@ -107,7 +103,7 @@ export default class ProfilePage extends Component {
                       </div>
                     </div>
                     <div className="card-body">
-                      <p className="card-text">{datasetId.description}</p>
+                      <p className="card-text">{this.state.datasets[datasetId].description}</p>
                     </div>
                   </div>
                 </div>
@@ -121,8 +117,8 @@ export default class ProfilePage extends Component {
             <div className="d-flex justify-content-around h3 row">
             My Subscriptions
             </div>
-
-            {this.state.datasetSubscriptions.map((datasetId, i) => {
+            {Object.entries(this.state.subscriptions).map((datasetId, i) => {
+            // {this.state.subscriptions.map((datasetId, i) => {
               return (
                 <div style={{ padding: "1rem" }}>
                   <div
@@ -140,11 +136,11 @@ export default class ProfilePage extends Component {
                     >
                       <div className="d-flex row">
                         <h5 style={{ flex: 1, padding: "0.5rem" }}>
-                          {datasetId.name}
+                          {this.state.datasets[datasetId[1].id].name}
                         </h5>
                         <div className="d-flex flex-row-reverse">
                           <Link
-                            to={`/details/${datasetId.name}`}
+                            to={`/details/${this.state.datasets[datasetId[1].id].name}`}
                             className="btn btn-secondary"
                             style={{
                               borderRadius: "20px",
@@ -158,9 +154,9 @@ export default class ProfilePage extends Component {
                       </div>
                     </div>
                     <div className="card-body">
-                      <p className="card-text">{datasetId.description}</p>
+                      <p className="card-text">{this.state.datasets[datasetId[1].id].description}</p>
                       <hr/>
-                      <p className="card-text">Subscription ends on:  {this.state.subscriptions[datasetId.id].endDate}
+                      <p className="card-text">Subscription ends on:  {datasetId.endDate}
                       </p>
                     </div>
                   </div>
